3fea0f27c4b7e3cbbb4b9dfad92cf19ce7a56082
Yamisagi <erencandan20@gmail.com>
2023-11-20 14:51:37 +0300

    feat: Solve Example1 & Create State etc.

--- src/pages/Example1.jsx
+++ src/pages/Example1.jsx
@@ -1,14 +1,46 @@
 import React, { useState } from 'react';
 
 const Example1 = () => {
+  const [widthHeight, setWidthHeight] = useState({
+    width: 200,
+    height: 200,
+  });
+
+  const handleIncrease = () => {
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    console.log(widthHeight.width);
+    console.log(widthHeight.height);
+    if (
+      widthHeight.width < window.innerWidth - 300 &&
+      widthHeight.height < window.innerHeight - 300
+    ) {
+      setWidthHeight({
+        width: widthHeight.width + 10,
+        height: widthHeight.height + 10,
+      });
+    } else {
+      alert('Maximum size reached!');
+      setWidthHeight({
+        width: 200,
+        height: 200,
+      });
+    }
+  };
   return (
     <div className='flex-container'>
-      <button
-        className='btn'
-      >
-        
+      <button onClick={handleIncrease} className='btn'>
+        <span>+</span>
       </button>
-      <div></div>
+      <div
+        style={{
+          marginTop: '20px',
+          backgroundColor: 'black',
+          width: `${widthHeight.width}px`,
+          height: `${widthHeight.height}px`,
+          borderRadius: '10px',
+        }}
+      ></div>
     </div>
   );
 };
