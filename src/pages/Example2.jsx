7d379e26feaace9d9008c9043ef6b765346bf81d
Yamisagi <erencandan20@gmail.com>
2023-11-20 14:56:59 +0300

    feat: Add random number generator functionality to Example2 component

--- src/pages/Example2.jsx
+++ src/pages/Example2.jsx
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 
 const Example2 = () => {
-  return <div>Example2</div>;
+  const [randomNumbers, setRandomNumbers] = useState([]);
+  // unique random number generator
+
+  const handleAddRandomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    if (!randomNumbers.includes(randomNumber)) {
+      setRandomNumbers((prevList) => prevList.concat(randomNumber));
+      console.log(randomNumbers.length);
+    } else if (randomNumbers.length >= 32) {
+      alert('Maximum number of numbers reached!');
+      setRandomNumbers([]);
+    } else {
+      return;
+    }
+  };
+  return (
+    <div className='flex-container'>
+      <button className='btn' onClick={handleAddRandomNumber}>
+        Add Random Number
+      </button>
+      <ul>
+        {randomNumbers.map((randomNumber) => (
+          <li key={randomNumber}>{randomNumber}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Example2;
